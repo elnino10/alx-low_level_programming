d43c4895dab1984b06a93765a46661224bae5675
Joe Egboka <joeegboka@gmail.com>
2023-07-13 22:20:59 +0000

    print to stderr without printf or put

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<unistd.h>
+#include<string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char errMsg[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, &errMsg, sizeof(errMsg) - 1);
+	return (1);
+}
