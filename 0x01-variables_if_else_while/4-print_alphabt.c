28aaf05084cad5ef985c359407e1dec8f560ef54
Joe Egboka <joeegboka@gmail.com>
2023-07-14 18:57:09 +0000

    print alphabets without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets without e and q
+ *
+ * Return - return 0 (success)
+ */
+int main(void)
+{
+	char alpha[] = {'a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+'m', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	int i;
+
+	for (i = 0; i < 24; i++)
+		putchar(alpha[i]);
+	putchar('\n');
+	return (0);
+}
