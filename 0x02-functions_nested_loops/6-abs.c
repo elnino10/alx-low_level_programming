ca87e5f7db6436e4f2703d02c6e55f602d9c1426
Joe Egboka <joeegboka@gmail.com>
2023-07-18 21:38:13 +0000

    return absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of any given number
+ * @int: type of expected parameter
+ * @a: value to be checked
+ * Return: returns an unsigned integer value
+ */
+int _abs(int a)
+{
+	int val = a;
+
+	if (val > 0)
+		return (val);
+	else if (val < 0)
+		return (-val);
+	else
+		return (0);
+}
