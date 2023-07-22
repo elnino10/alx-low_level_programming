2f2baf23ac919edbcb6e3ccce57a166bc7610a0d
Joe Egboka <joeegboka@gmail.com>
2023-07-22 01:26:18 +0000

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,6 +5,8 @@ int isPrime(int num);
 /**
  * main - finds largest prime factor of a number and prints it
  * Return: returns 0 if successful
+ * largestPrimeFactor: checks for largest prime factor
+ * isPrime: checks if the provided number is prime
  */
 
 int main(void)
@@ -13,38 +15,44 @@ int main(void)
 	int largestPrime = largestPrimeFactor(number);
 
 	printf("%d\n", largestPrime);
-	return 0;
+	return (0);
 }
 
-int isPrime(int num) {
+int isPrime(int num)
+{
 	int i;
 
 	if (num <= 1)
-		return 0;
+		return (0);
 
-	for (i = 2; i * i <= num; i++) {
+	for (i = 2; i * i <= num; i++)
+	{
 		if (num % i == 0)
-			return 0;
+			return (0);
 	}
 
-	return 1;
+	return (1);
 }
 
-int largestPrimeFactor(int num) {
+int largestPrimeFactor(int num)
+{
 	int i;
 	int largestFactor = 1;
 
-	while (num % 2 == 0) {
+	while (num % 2 == 0)
+	{
 		largestFactor = 2;
 		num /= 2;
 	}
 
-	for (i = 3; i <= num; i += 2) {
-		while (num % i == 0) {
+	for (i = 3; i <= num; i += 2)
+	{
+		while (num % i == 0)
+		{
 			largestFactor = i;
 			num /= i;
 		}
 	}
 
-	return largestFactor;
+	return (largestFactor);
 }
