f13674db786caf9c73e4079c14fcbdd92aa1f5d7
Joe Egboka <joeegboka@gmail.com>
2023-07-28 21:52:06 +0000

    magic file and empty infinite add

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+int _strlen(char *);
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: pointer to first number
+ * @n2: pointer to second number
+ * @r: pointer to result buffer
+ * @size_r: size of buffer
+ * Return: pointer to the result
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+}
