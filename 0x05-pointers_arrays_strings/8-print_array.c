28e651b53771744ad7a85286bb38338ac60eb16d
Joe Egboka <joeegboka@gmail.com>
2023-07-30 19:40:00 +0000

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,20 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i != n - 1)
-			printf("%d, ", *(a + i));
-		else if (i == n - 1 || n == 1)
-			printf("%d\n", *(a + i));
-		else if (n <= 0)
-			return;
+		if (n > 1)
+		{
+			if (i != n - 1)
+				printf("%d, ", *(a + i));
+			else
+				printf("%d\n", *(a + i));
+		}
+		else if (n == 1)
+		{
+			printf("%d\n", *a);
+		}
+		else
+		{
+			printf("\n");
+		}
 	}
 }
