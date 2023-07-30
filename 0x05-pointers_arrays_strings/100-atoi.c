b8fae0acf120c3c53ef67d4bda6bc4aaa2892ea4
Joe Egboka <joeegboka@gmail.com>
2023-07-30 22:50:12 +0000

    atoi function

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,13 +7,27 @@
  */
 int _atoi(char *s)
 {
-	int i;
+	int i, val, flag;
 	unsigned int res;
 
+	flag = 0;
 	res = 0;
-	for (i = 0; s[i] != '\0'; i++)
+	val = 1;
+	i = 0;
+	while (s[i] != '\0')
 	{
-		res = res * 10 + s[i] - '0';
+		if (s[i] == '-')
+			val *= -1;
+		while (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			res = res * 10 + s[i] - '0';
+			i++;
+		}
+		if (flag)
+			break;
+		i++;
 	}
+	res *= val;
 	return (res);
 }
