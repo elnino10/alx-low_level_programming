e637343b524985a65d60321e71c894769d7c5729
Joe Egboka <joeegboka@gmail.com>
2023-08-01 02:31:30 +0000

    modify strchr function

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,18 +8,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	char *ptr;
-
-	ptr = s;
 	while (*s != '\0')
 	{
 		if (*s == c)
 			return (s);
-
-		if (c == '\0')
-			*s = '\0';
-
 		s++;
 	}
-	return (ptr);
+	if (c == '\0')
+		return ('\0');
+	return ('\0');
 }
