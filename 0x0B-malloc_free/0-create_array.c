d5dbcfd12ad5755199b37e8670f051dc95147f6e
Joe Egboka <joeegboka@gmail.com>
2023-08-07 13:32:07 +0000

    edit malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,13 +13,12 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *s;
 
-	s = malloc(size);
-	if (size == 0)
-		return ('\0');
+	s = malloc(sizeof(char) * size);
+	if (size == 0 || s == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; ++i)
 		s[i] = c;
 
-	s[i] = '\0';
 	return (s);
 }
