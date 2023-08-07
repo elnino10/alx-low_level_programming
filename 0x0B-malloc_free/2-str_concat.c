6a99c769ae9b06e47d657847adf70ab74bb4b765
Joe Egboka <joeegboka@gmail.com>
2023-08-07 17:57:32 +0000

    refactor 2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,8 +14,7 @@ char *str_concat(char *s1, char *s2)
 
 	i = j = k = len = 0;
 	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-
+		len = 0;
 	if (s1 != NULL)
 	{
 		while (s1[i])
@@ -28,25 +27,20 @@ char *str_concat(char *s1, char *s2)
 	}
 	len = i + j;
 	ptr = malloc(sizeof(char) * (len + 1));
-
 	if (ptr == NULL)
 		return (NULL);
 	if (s1 != NULL)
 	{
 		while (*s1)
 		{
-			ptr[k] = *s1;
-			s1++;
-			k++;
+			ptr[k++] = *s1++;
 		}
 	}
 	if (s2 != NULL)
 	{
 		while (*s2)
 		{
-			ptr[k] = *s2;
-			s2++;
-			k++;
+			ptr[k++] = *s2++;
 		}
 	}
 	ptr[k + 1] = '\0';
