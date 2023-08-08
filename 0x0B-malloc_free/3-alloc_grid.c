d7762d7a452dc93e27d855f8c77a768a58af20ed
Joe Egboka <joeegboka@gmail.com>
2023-08-08 10:22:39 +0100

    modify file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,11 @@ int **alloc_grid(int width, int height)
 	{
 		ptr[i] = malloc(sizeof(int) * width);
 		if (ptr[i] == NULL)
+		{
+			for (j = 0; j < width; ++j)
+				free(ptr[j]);
 			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; ++i)
