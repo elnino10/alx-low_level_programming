81b7802c7ba911bde3b32fb02a43bd7120d6064b
Joe Egboka <joeegboka@gmail.com>
2023-08-15 00:17:27 +0100

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,14 +6,13 @@
  * @name: dog name
  * @age: dog age
  * @owner: dog owner
+ * Return: new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *n_dog;
 
-	n_dog = malloc(sizeof(dog_t));
-	if (n_dog == NULL)
-		return (NULL);
+	n_dog = malloc(sizeof(struct dog));
 	n_dog->name = name;
 	n_dog->age = age;
 	n_dog->owner = owner;
