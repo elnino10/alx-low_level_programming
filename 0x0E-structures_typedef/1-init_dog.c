9f0817dc955a36436d5eba76ca8072b4a42d61ac
Joe Egboka <joeegboka@gmail.com>
2023-08-14 17:37:05 +0100

    1-init_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,12 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *ptr = d;
-
-	if (ptr != NULL)
+	if (d != NULL)
 	{
-		ptr->name = name;
-		ptr->age = age;
-		ptr->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
