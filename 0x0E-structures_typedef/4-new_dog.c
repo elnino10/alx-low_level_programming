e49a2bef032c3a63a6e07dd7d7959deea9ac3ee2
Joe Egboka <joeegboka@gmail.com>
2023-08-15 00:44:09 +0100

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "dog.h"
+char *_strdup(char *);
 
 /**
  * new_dog - creates a new dog
@@ -11,12 +12,59 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *n_dog;
+	char *n_name;
+	char *n_owner;
 
 	n_dog = malloc(sizeof(struct dog));
 	if (n_dog == NULL)
 		return (NULL);
+
+	n_name = _strdup(name);
+	if (n_name == NULL)
+	{
+		free(n_dog);
+		return (NULL);
+	}
+
+	n_owner = _strdup(owner);
+	if (n_owner == NULL)
+	{
+		free(n_dog);
+		free(n_name);
+		return (NULL);
+	}
+
 	n_dog->name = name;
 	n_dog->age = age;
 	n_dog->owner = owner;
+
 	return (n_dog);
+}
+
+/**
+ * _strdup - creates a duplicate for string
+ * @s: pointer to string to be copied
+ * Return: a pointer to the duplicated string or NULL when either
+ * str == NULL or memory could not be allocated
+ */
+char *_strdup(char *s)
+{
+	char *arr;
+	unsigned int i, j;
+
+	i = j = 0;
+	if (s == NULL)
+		return (NULL);
+	while (s[i])
+		i++;
+	arr = malloc(sizeof(char) * (i + 1));
+	if (arr == NULL)
+		return (NULL);
+	while (s[j])
+	{
+		arr[j] = s[j];
+		j++;
+	}
+	arr[j] = 0;
+	return (arr);
 }
