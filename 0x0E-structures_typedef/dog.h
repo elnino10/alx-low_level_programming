b328f67fc5dc45ca88a5370861d35b2ec021c97e
Joe Egboka <joeegboka@gmail.com>
2023-08-14 16:58:02 +0100

    modify dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef HEADER_DOG
 #define HEADER_DOG
 
-struct dog 
+/**
+ * struct dog - dog struct declaration
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: dog owner
+ */
+struct dog
 {
 	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 
 #endif
