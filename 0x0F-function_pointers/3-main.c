8d686fb179f328a5d4d93d1fdf3d3ad4cdc385e1
Joe Egboka <joeegboka@gmail.com>
2023-08-16 23:53:42 +0100

    create a program that does simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+
+/**
+ * main - controls simple operations
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int res, num1, num2;
+	int (*op_func)(int, int);
+
+	if (argc == 4)
+	{
+		if (argv[2][1] != '\0')
+		{
+			printf("Error\n");
+			exit(99);
+		}
+		op_func = get_op_func(argv[2]);
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[3]);
+
+		res = op_func(num1, num2);
+		printf("%d\n", res);
+	}
+	else
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sums a and b
+ * @a: integer element to add
+ * @b: integer element to add
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts b from a
+ * @a: integer element to subtract
+ * @b: integer element to subtract
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * op_mul - multiplies a and b
+ * @a: integer element to multiply
+ * @b: integer element to multiply
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * op_div - divides a by b
+ * @a: integer element to be divided
+ * @b: integer element that divides a
+ * Return: result of division of a and b
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+        return (a / b);
+}
+
+/**
+ * op_mod - gets the remainder of the division of a by b
+ * @a: integer element to be divided
+ * @b: integer element that divides a
+ * Return:  returns the remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+        return (a % b);
+}
+
