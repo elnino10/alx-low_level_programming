acd964dae7f557663ec08710dc899eedaf05e823
Joe Egboka <joeegboka@gmail.com>
2023-08-24 15:56:42 +0100

    add 1-list_len.c and update lists.h

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * list_len - gets the number of elements in a linked list_t list
+ * @h: pointer to the head node
+ *
+ * Return: number of elements in list
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *temp = h;
+	size_t len = 0;
+
+	if (temp == NULL)
+	{
+		return (len);
+	}
+	else
+	{
+		while (temp)
+		{
+			len++;
+			temp = temp->next;
+		}
+	}
+    return (len);
+}
