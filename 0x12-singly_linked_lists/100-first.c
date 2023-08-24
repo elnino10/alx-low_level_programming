c62daa652899f415fc349b226873748b18e6a438
Joe Egboka <joeegboka@gmail.com>
2023-08-24 22:50:46 +0100

    add 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,12 @@
 #include "lists.h"
 
-void startup_call(void) __attribute__((constructor));
-
 /**
  * startup_call - prints to the standard output before main function
  *
  * Return: nothing
  */
-void startup_call(void)
+void __attribute__((constructor)) startup_call(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my\
-back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my ");
+	printf("house upon my back!\n");
 }
