7c36839bfa1679616f7b4a5621951f56ea27f58f
Joe Egboka <joeegboka@gmail.com>
2023-10-16 16:50:26 +0100

    100-operations.so

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+
+/**
+ * add - adds two integers
+ * @a: integer to be added
+ * @b: integer to be added to a
+ *
+ * Return: sum of the two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts b from a
+ * @a: integer from which b will be subtracted
+ * @b: integer to be subtracted from a
+ *
+ * Return: difference of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two intergers
+ * @a: integer to be multiplied
+ * @b: integer by which to be multiplied
+ *
+ * Return: product of the two intergers
+ */
+int mul(int a,  int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides one integer by another
+ * @a: integer to be divided
+ * @b: integer by which a will be divided
+ *
+ * Return: quotient of the two intergers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - finds modulus of a number
+ * @a: integer to be divided
+ * @b: integer by which a will be divided
+ *
+ * Return: modulus of a by b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
