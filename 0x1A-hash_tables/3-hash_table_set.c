dc1d865b767fa246a77a525d6bda96ec11729b53
Joe Egboka <joeegboka@gmail.com>
2023-11-04 14:05:21 +0100

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,14 +13,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL, *curr_node;
 	unsigned long int index;
 
-	/*create item node with key, value and next node*/
+	if (!ht || !key)
+		return (0);
+
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
-	if (!key)
-		return (0);
+
 	new_node->key = strdup(key);
-	/*index where node will be stored*/
 	index = key_index((const unsigned char *)key, ht->size);
 	if (!ht->array[index])
 	{
