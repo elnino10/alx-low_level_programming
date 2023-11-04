16271c8bbbca34cdde48cef498d48f9c86b033d6
Joe Egboka <joeegboka@gmail.com>
2023-11-04 05:21:23 +0100

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
-
 	if (!key)
 		return (0);
 	new_node->key = strdup(key);
@@ -25,29 +24,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *)key, ht->size);
 	if (!ht->array[index])
 	{
-		/*sets node at position if empty*/
-		new_node->value = strdup(value);
+		new_node->value = strdup(value); /*sets node at position if empty*/
 		new_node->next = NULL;
 		ht->array[index] = new_node;
 	}
 	else
 	{
 		curr_node = ht->array[index];
-		while (curr_node->next)
+		if (strcmp(curr_node->key, key) == 0)
 		{
-			if (strcmp(curr_node->key, key) == 0)
+			while (curr_node)
 			{
-				/*update value to new value*/
 				free(curr_node->value);
 				curr_node->value = strdup(value);
 				return (1);
+				curr_node = curr_node->next;
 			}
-			curr_node = curr_node->next;
 		}
-		/*make node the first node at array[index]*/
-		new_node->value = strdup(value);
-		new_node->next = ht->array[index];
-		ht->array[index] = new_node;
+		else
+		{
+			new_node->value = strdup(value); /*make node first at array[index]*/
+			new_node->next = ht->array[index];
+			ht->array[index] = new_node;
+		}
 	}
 	return (1);
 }
