983706957a626059b3d75aac3c7067ab45f792cf
Joe Egboka <joeegboka@gmail.com>
2023-11-05 09:46:55 +0100

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
-	new_node->key = (char *)key;
+	new_node->key = strdup(key);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	if (!ht->array[index])
