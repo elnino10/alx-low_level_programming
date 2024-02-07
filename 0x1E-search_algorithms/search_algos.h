38726e5bb0cb11f31ceaa2b7b389ad0000ab965d
Joe Egboka <joeegboka@gmail.com>
2024-02-08 00:56:49 +0100

    update header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,10 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /*function prototypes*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 /* helper functions*/
 int binary_search_recurs(int *array, size_t lb, size_t ub, int value);
